0d2b3f5d5c77713c3a8674840ac34cb118f53230
a <a>
2025-11-25 18:29:48 +0100

    a

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-// Constantes de precios
-
 const PRECIO_HORA = 10.85;
 
 // Pluses
@@ -7,6 +5,13 @@ const PRECIO_HORA = 10.85;
 const PLUS_FESTIVO_ESPECIAL = PRECIO_HORA * 0.75;
 const PLUS_NOCTURNIDAD = PRECIO_HORA * 0.75 * 0.25;
 const PLUS_EVENTO = 3;
+
+// Cotizaciones
+
+const CONTINGENCIAS_COMUNES = 0.047;
+const DESEMPLEO = 0.0155;
+const FORMACION_PROFESIONAL = 0.001;
+const MECANISMO_EQUIDAD_INTERGENERACIONAL = 0.0013;
 
 // Elementos del DOM
 
@@ -18,6 +23,7 @@ const TURNOS_5_HORAS = document.getElementById('5-horas');
 const TURNOS_6_5_HORAS = document.getElementById('6.5-horas');
 const TURNOS_7_HORAS = document.getElementById('7-horas');
 const TURNOS_8_HORAS = document.getElementById('8-horas');
+const COACHING = document.getElementById('coaching');
 const FESTIVO_ESPECIAL = document.getElementById('festivo-especial');
 const EVENTO = document.getElementById('evento');
 
@@ -28,21 +34,22 @@ const calcularSalario = () => {
 	const SALARIO_6_5_HORAS = TURNOS_6_5_HORAS.value * PRECIO_HORA * 6.5;
 	const SALARIO_7_HORAS = TURNOS_7_HORAS.value * PRECIO_HORA * 7;
 	const SALARIO_8_HORAS = TURNOS_8_HORAS.value * PRECIO_HORA * 8 + PLUS_NOCTURNIDAD * TURNOS_8_HORAS.value;
+	const SALARIO_COACHING = COACHING.value * PRECIO_HORA;
 
 	const SALARIO_FESTIVO_ESPECIAL = PLUS_FESTIVO_ESPECIAL * FESTIVO_ESPECIAL.value;
-
 	const SALARIO_EVENTO = EVENTO.value * PRECIO_HORA + PLUS_EVENTO * EVENTO.value;
 
-	const SALARIO_BRUTO = SALARIO_4_HORAS + SALARIO_5_HORAS + SALARIO_6_5_HORAS + SALARIO_7_HORAS + SALARIO_8_HORAS + SALARIO_FESTIVO_ESPECIAL + SALARIO_EVENTO;
+	const SALARIO_BRUTO = SALARIO_4_HORAS + SALARIO_5_HORAS + SALARIO_6_5_HORAS + SALARIO_7_HORAS + SALARIO_8_HORAS + SALARIO_COACHING + SALARIO_FESTIVO_ESPECIAL + SALARIO_EVENTO;
 
 	OUTPUT.innerHTML =
-		`<p>Salario por turnos:</p>
+		`<p>Salario desglosado:</p>
 
 			<p>-4 horas: ${SALARIO_4_HORAS} €.</p>
 			<p>-5 horas: ${SALARIO_5_HORAS} €.</p>
 			<p>-6.5 horas: ${SALARIO_6_5_HORAS} €.</p>
 			<p>-7 horas: ${SALARIO_7_HORAS} €.</p>
 			<p>-8 horas: ${SALARIO_8_HORAS} €.</p>
+			<p>-Coaching: ${SALARIO_COACHING} €.</p>
 			<p>-Festivo especial: ${SALARIO_FESTIVO_ESPECIAL} €.</p>
 			<p>-Eventos: ${SALARIO_EVENTO} €.</p>
 
