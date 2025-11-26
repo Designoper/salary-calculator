11f4d0d15b4e20d0b6b31ef067ce2d466de1ba51
a <a>
2025-11-26 11:20:30 +0100

    a

--- js/index.js
+++ js/index.js
@@ -1,25 +1,50 @@
 const PRECIO_HORA = 10.85;
 
-// Pluses
-
-const PLUS_FESTIVO_ESPECIAL = PRECIO_HORA * 0.75;
-const PLUS_NOCTURNIDAD = PRECIO_HORA * 0.75 * 0.25;
-const PLUS_EVENTO = 3;
-
-// Elementos formulario
-
 const BOTON_CALCULAR = document.querySelector('form button');
 const OUTPUT = document.querySelector('form output');
 
-const TURNOS_4_HORAS = document.getElementById('4-horas');
-const TURNOS_5_HORAS = document.getElementById('5-horas');
-const TURNOS_6_5_HORAS = document.getElementById('6.5-horas');
-const TURNOS_7_HORAS = document.getElementById('7-horas');
-const TURNOS_8_HORAS = document.getElementById('8-horas');
+const calcularSalarioBruto = () => {
+	const PLUS_FESTIVO_ESPECIAL = PRECIO_HORA * 0.75;
+	const PLUS_NOCTURNIDAD = PRECIO_HORA * 0.75 * 0.25;
+	const PLUS_EVENTO = 3;
 
-const COACHING = document.getElementById('coaching');
-const FESTIVO_ESPECIAL = document.getElementById('festivo-especial');
-const EVENTO = document.getElementById('evento');
+	const TURNOS_4_HORAS = document.getElementById('4-horas');
+	const TURNOS_5_HORAS = document.getElementById('5-horas');
+	const TURNOS_6_5_HORAS = document.getElementById('6.5-horas');
+	const TURNOS_7_HORAS = document.getElementById('7-horas');
+	const TURNOS_8_HORAS = document.getElementById('8-horas');
+
+	const COACHING = document.getElementById('coaching');
+	const FESTIVO_ESPECIAL = document.getElementById('festivo-especial');
+	const EVENTO = document.getElementById('evento');
+
+	const SALARIO_4_HORAS = TURNOS_4_HORAS.value * PRECIO_HORA * 4;
+	const SALARIO_5_HORAS = TURNOS_5_HORAS.value * PRECIO_HORA * 5;
+	const SALARIO_6_5_HORAS = TURNOS_6_5_HORAS.value * PRECIO_HORA * 6.5;
+	const SALARIO_7_HORAS = TURNOS_7_HORAS.value * PRECIO_HORA * 7;
+	const SALARIO_8_HORAS = TURNOS_8_HORAS.value * PRECIO_HORA * 8 + PLUS_NOCTURNIDAD * TURNOS_8_HORAS.value;
+	const SALARIO_COACHING = COACHING.value * PRECIO_HORA;
+
+	const SALARIO_FESTIVO_ESPECIAL = PLUS_FESTIVO_ESPECIAL * FESTIVO_ESPECIAL.value;
+	const SALARIO_EVENTO = EVENTO.value * PRECIO_HORA + PLUS_EVENTO * EVENTO.value;
+
+	const VACACIONES = calcularVacaciones();
+
+	const SALARIO_BRUTO = SALARIO_4_HORAS + SALARIO_5_HORAS + SALARIO_6_5_HORAS + SALARIO_7_HORAS + SALARIO_8_HORAS + SALARIO_COACHING + SALARIO_FESTIVO_ESPECIAL + SALARIO_EVENTO + VACACIONES;
+
+	return {
+		SALARIO_4_HORAS,
+		SALARIO_5_HORAS,
+		SALARIO_6_5_HORAS,
+		SALARIO_7_HORAS,
+		SALARIO_8_HORAS,
+		SALARIO_COACHING,
+		SALARIO_FESTIVO_ESPECIAL,
+		SALARIO_EVENTO,
+		VACACIONES,
+		SALARIO_BRUTO
+	};
+}
 
 const calcularCotizaciones = (salarioBruto) => {
 	const CONTINGENCIAS_COMUNES = 0.047;
@@ -61,40 +86,28 @@ const calcularVacaciones = () => {
 	return remuneracionVacaciones;
 }
 
-const calcularSalario = () => {
+const calcularNomina = () => {
 
-	const SALARIO_4_HORAS = TURNOS_4_HORAS.value * PRECIO_HORA * 4;
-	const SALARIO_5_HORAS = TURNOS_5_HORAS.value * PRECIO_HORA * 5;
-	const SALARIO_6_5_HORAS = TURNOS_6_5_HORAS.value * PRECIO_HORA * 6.5;
-	const SALARIO_7_HORAS = TURNOS_7_HORAS.value * PRECIO_HORA * 7;
-	const SALARIO_8_HORAS = TURNOS_8_HORAS.value * PRECIO_HORA * 8 + PLUS_NOCTURNIDAD * TURNOS_8_HORAS.value;
-	const SALARIO_COACHING = COACHING.value * PRECIO_HORA;
+	const SALARIO_BRUTO = calcularSalarioBruto();
 
-	const SALARIO_FESTIVO_ESPECIAL = PLUS_FESTIVO_ESPECIAL * FESTIVO_ESPECIAL.value;
-	const SALARIO_EVENTO = EVENTO.value * PRECIO_HORA + PLUS_EVENTO * EVENTO.value;
+	const COTIZACION = calcularCotizaciones(SALARIO_BRUTO.SALARIO_BRUTO);
 
-	const VACACIONES = calcularVacaciones();
-
-	const SALARIO_BRUTO = SALARIO_4_HORAS + SALARIO_5_HORAS + SALARIO_6_5_HORAS + SALARIO_7_HORAS + SALARIO_8_HORAS + SALARIO_COACHING + SALARIO_FESTIVO_ESPECIAL + SALARIO_EVENTO + VACACIONES;
-
-	const COTIZACION = calcularCotizaciones(SALARIO_BRUTO);
-
-	const SALARIO_NETO = SALARIO_BRUTO - COTIZACION.TOTAL_COTIZACIONES;
+	const SALARIO_NETO = SALARIO_BRUTO.SALARIO_BRUTO - COTIZACION.TOTAL_COTIZACIONES;
 
 	OUTPUT.innerHTML =
 		`<p>Salario desglosado:</p>
 
-			<p>-4 horas: ${SALARIO_4_HORAS} €.</p>
-			<p>-5 horas: ${SALARIO_5_HORAS} €.</p>
-			<p>-6.5 horas: ${SALARIO_6_5_HORAS} €.</p>
-			<p>-7 horas: ${SALARIO_7_HORAS} €.</p>
-			<p>-8 horas: ${SALARIO_8_HORAS} €.</p>
-			<p>-Coaching: ${SALARIO_COACHING} €.</p>
-			<p>-Festivo especial: ${SALARIO_FESTIVO_ESPECIAL} €.</p>
-			<p>-Eventos: ${SALARIO_EVENTO} €.</p>
-			<p>-Vacaciones: ${VACACIONES} €.</p>
+			<p>-4 horas: ${SALARIO_BRUTO.SALARIO_4_HORAS} €.</p>
+			<p>-5 horas: ${SALARIO_BRUTO.SALARIO_5_HORAS} €.</p>
+			<p>-6.5 horas: ${SALARIO_BRUTO.SALARIO_6_5_HORAS} €.</p>
+			<p>-7 horas: ${SALARIO_BRUTO.SALARIO_7_HORAS} €.</p>
+			<p>-8 horas: ${SALARIO_BRUTO.SALARIO_8_HORAS} €.</p>
+			<p>-Coaching: ${SALARIO_BRUTO.SALARIO_COACHING} €.</p>
+			<p>-Festivo especial: ${SALARIO_BRUTO.SALARIO_FESTIVO_ESPECIAL} €.</p>
+			<p>-Eventos: ${SALARIO_BRUTO.SALARIO_EVENTO} €.</p>
+			<p>-Vacaciones: ${SALARIO_BRUTO.VACACIONES} €.</p>
 
-			<p>Salario bruto: ${SALARIO_BRUTO} €.</p>
+			<p>Salario bruto: ${SALARIO_BRUTO.SALARIO_BRUTO} €.</p>
 
 			<p>--Cotizaciones--</p>
 
@@ -108,4 +121,4 @@ const calcularSalario = () => {
 		`;
 }
 
-BOTON_CALCULAR.onclick = () => calcularSalario();
+BOTON_CALCULAR.onclick = () => calcularNomina();
