2a230a108332fafa12a85b53bb92a0ec0ae8139a
a <a>
2025-11-26 11:03:12 +0100

    a

--- js/index.js
+++ js/index.js
@@ -6,13 +6,6 @@ const PLUS_FESTIVO_ESPECIAL = PRECIO_HORA * 0.75;
 const PLUS_NOCTURNIDAD = PRECIO_HORA * 0.75 * 0.25;
 const PLUS_EVENTO = 3;
 
-// Cotizaciones
-
-const CONTINGENCIAS_COMUNES = 0.047;
-const DESEMPLEO = 0.0155;
-const FORMACION_PROFESIONAL = 0.001;
-const MECANISMO_EQUIDAD_INTERGENERACIONAL = 0.0013;
-
 // Elementos formulario
 
 const BOTON_CALCULAR = document.querySelector('form button');
@@ -23,9 +16,50 @@ const TURNOS_5_HORAS = document.getElementById('5-horas');
 const TURNOS_6_5_HORAS = document.getElementById('6.5-horas');
 const TURNOS_7_HORAS = document.getElementById('7-horas');
 const TURNOS_8_HORAS = document.getElementById('8-horas');
+
 const COACHING = document.getElementById('coaching');
 const FESTIVO_ESPECIAL = document.getElementById('festivo-especial');
 const EVENTO = document.getElementById('evento');
+
+const calcularCotizaciones = (salarioBruto) => {
+	const CONTINGENCIAS_COMUNES = 0.047;
+	const DESEMPLEO = 0.0155;
+	const FORMACION_PROFESIONAL = 0.001;
+	const MECANISMO_EQUIDAD_INTERGENERACIONAL = 0.0013;
+
+	const COTIZACION_CONTINGENCIAS_COMUNES = salarioBruto * CONTINGENCIAS_COMUNES;
+	const COTIZACION_DESEMPLEO = salarioBruto * DESEMPLEO;
+	const COTIZACION_FORMACION_PROFESIONAL = salarioBruto * FORMACION_PROFESIONAL;
+	const COTIZACION_MECANISMO_EQUIDAD_INTERGENERACIONAL = salarioBruto * MECANISMO_EQUIDAD_INTERGENERACIONAL;
+
+	const TOTAL_COTIZACIONES = COTIZACION_CONTINGENCIAS_COMUNES + COTIZACION_DESEMPLEO + COTIZACION_FORMACION_PROFESIONAL + COTIZACION_MECANISMO_EQUIDAD_INTERGENERACIONAL;
+
+	return {
+		COTIZACION_CONTINGENCIAS_COMUNES,
+		COTIZACION_DESEMPLEO,
+		COTIZACION_FORMACION_PROFESIONAL,
+		COTIZACION_MECANISMO_EQUIDAD_INTERGENERACIONAL,
+		TOTAL_COTIZACIONES
+	};
+}
+
+const calcularVacaciones = () => {
+	const DIAS_SEMANA = 7;
+	const DIAS_VACACIONES = document.getElementById('vacaciones');
+	const HORAS_SEMANA_CONTRATO = document.getElementById('horas-semana');
+	let remuneracionVacaciones;
+
+	if (DIAS_VACACIONES.value === 0) {
+		remuneracionVacaciones = 0;
+	}
+
+	else {
+		const HORAS_VACACIONES = (HORAS_SEMANA_CONTRATO.value / DIAS_SEMANA) * DIAS_VACACIONES.value;
+		remuneracionVacaciones = HORAS_VACACIONES * PRECIO_HORA;
+	}
+
+	return remuneracionVacaciones;
+}
 
 const calcularSalario = () => {
 
@@ -39,16 +73,13 @@ const calcularSalario = () => {
 	const SALARIO_FESTIVO_ESPECIAL = PLUS_FESTIVO_ESPECIAL * FESTIVO_ESPECIAL.value;
 	const SALARIO_EVENTO = EVENTO.value * PRECIO_HORA + PLUS_EVENTO * EVENTO.value;
 
-	const SALARIO_BRUTO = SALARIO_4_HORAS + SALARIO_5_HORAS + SALARIO_6_5_HORAS + SALARIO_7_HORAS + SALARIO_8_HORAS + SALARIO_COACHING + SALARIO_FESTIVO_ESPECIAL + SALARIO_EVENTO;
+	const VACACIONES = calcularVacaciones();
 
-	const COTIZACION_CONTINGENCIAS_COMUNES = SALARIO_BRUTO * CONTINGENCIAS_COMUNES;
-	const COTIZACION_DESEMPLEO = SALARIO_BRUTO * DESEMPLEO;
-	const COTIZACION_FORMACION_PROFESIONAL = SALARIO_BRUTO * FORMACION_PROFESIONAL;
-	const COTIZACION_MECANISMO_EQUIDAD_INTERGENERACIONAL = SALARIO_BRUTO * MECANISMO_EQUIDAD_INTERGENERACIONAL;
+	const SALARIO_BRUTO = SALARIO_4_HORAS + SALARIO_5_HORAS + SALARIO_6_5_HORAS + SALARIO_7_HORAS + SALARIO_8_HORAS + SALARIO_COACHING + SALARIO_FESTIVO_ESPECIAL + SALARIO_EVENTO + VACACIONES;
 
-	const TOTAL_COTIZACIONES = COTIZACION_CONTINGENCIAS_COMUNES + COTIZACION_DESEMPLEO + COTIZACION_FORMACION_PROFESIONAL + COTIZACION_MECANISMO_EQUIDAD_INTERGENERACIONAL;
+	const COTIZACION = calcularCotizaciones(SALARIO_BRUTO);
 
-	const SALARIO_NETO = SALARIO_BRUTO - TOTAL_COTIZACIONES;
+	const SALARIO_NETO = SALARIO_BRUTO - COTIZACION.TOTAL_COTIZACIONES;
 
 	OUTPUT.innerHTML =
 		`<p>Salario desglosado:</p>
@@ -61,16 +92,17 @@ const calcularSalario = () => {
 			<p>-Coaching: ${SALARIO_COACHING} €.</p>
 			<p>-Festivo especial: ${SALARIO_FESTIVO_ESPECIAL} €.</p>
 			<p>-Eventos: ${SALARIO_EVENTO} €.</p>
+			<p>-Vacaciones: ${VACACIONES} €.</p>
 
 			<p>Salario bruto: ${SALARIO_BRUTO} €.</p>
 
 			<p>--Cotizaciones--</p>
 
-			<p>-Contingencias comunes: -${COTIZACION_CONTINGENCIAS_COMUNES} €.</p>
-			<p>-Desempleo: -${COTIZACION_DESEMPLEO} €.</p>
-			<p>-Formación profesional: -${COTIZACION_FORMACION_PROFESIONAL} €.</p>
-			<p>-Mecanismo de equidad intergeneracional: -${COTIZACION_MECANISMO_EQUIDAD_INTERGENERACIONAL} €.</p>
-			<p>Total cotizaciones: -${TOTAL_COTIZACIONES} €.</p>
+			<p>-Contingencias comunes: -${COTIZACION.COTIZACION_CONTINGENCIAS_COMUNES} €.</p>
+			<p>-Desempleo: -${COTIZACION.COTIZACION_DESEMPLEO} €.</p>
+			<p>-Formación profesional: -${COTIZACION.COTIZACION_FORMACION_PROFESIONAL} €.</p>
+			<p>-Mecanismo de equidad intergeneracional: -${COTIZACION.COTIZACION_MECANISMO_EQUIDAD_INTERGENERACIONAL} €.</p>
+			<p>Total cotizaciones: -${COTIZACION.TOTAL_COTIZACIONES} €.</p>
 
 			<p>Salario neto: ${SALARIO_NETO} €.</p>
 		`;
