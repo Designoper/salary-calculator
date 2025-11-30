c5fef8ee928e9434fbfa9e549207e8122ca5600b
a <a>
2025-11-30 09:39:05 +0100

    a

--- js/index.js
+++ js/index.js
@@ -9,8 +9,10 @@ const calcularSalarioBruto = () => {
 
 	// Plus adicionales
 
+	const PLUS_FESTIVO = PRECIO_HORA * 0.25;
 	const PLUS_FESTIVO_ESPECIAL = PRECIO_HORA * 0.75;
-	const PLUS_NOCTURNIDAD = PRECIO_HORA * 0.75 * 0.25;
+	const PLUS_NOCTURNIDAD_8_HORAS = PRECIO_HORA * 0.75 * 0.25;
+	const PLUS_NOCTURNIDAD_8_25_HORAS = PRECIO_HORA * 0.25;
 	const PLUS_EVENTO = 3;
 
 	// Obtener valores de los inputs
@@ -20,8 +22,10 @@ const calcularSalarioBruto = () => {
 	const TURNOS_6_5_HORAS = document.getElementById('6.5-horas');
 	const TURNOS_7_HORAS = document.getElementById('7-horas');
 	const TURNOS_8_HORAS = document.getElementById('8-horas');
+	const TURNOS_8_25_HORAS = document.getElementById('8.25-horas');
 
 	const COACHING = document.getElementById('coaching');
+	const FESTIVO = document.getElementById('festivo');
 	const FESTIVO_ESPECIAL = document.getElementById('festivo-especial');
 	const EVENTO = document.getElementById('evento');
 
@@ -31,15 +35,17 @@ const calcularSalarioBruto = () => {
 	const SALARIO_5_HORAS = TURNOS_5_HORAS.value * PRECIO_HORA * 5;
 	const SALARIO_6_5_HORAS = TURNOS_6_5_HORAS.value * PRECIO_HORA * 6.5;
 	const SALARIO_7_HORAS = TURNOS_7_HORAS.value * PRECIO_HORA * 7;
-	const SALARIO_8_HORAS = TURNOS_8_HORAS.value * PRECIO_HORA * 8 + PLUS_NOCTURNIDAD * TURNOS_8_HORAS.value;
+	const SALARIO_8_HORAS = TURNOS_8_HORAS.value * PRECIO_HORA * 8 + PLUS_NOCTURNIDAD_8_HORAS * TURNOS_8_HORAS.value;
+	const SALARIO_8_25_HORAS = TURNOS_8_25_HORAS.value * PRECIO_HORA * 8.25 + PLUS_NOCTURNIDAD_8_25_HORAS * TURNOS_8_25_HORAS.value;
 	const SALARIO_COACHING = COACHING.value * PRECIO_HORA;
 
+	const SALARIO_FESTIVO = PLUS_FESTIVO * FESTIVO.value;
 	const SALARIO_FESTIVO_ESPECIAL = PLUS_FESTIVO_ESPECIAL * FESTIVO_ESPECIAL.value;
 	const SALARIO_EVENTO = EVENTO.value * PRECIO_HORA + PLUS_EVENTO * EVENTO.value;
 
 	const VACACIONES = calcularVacaciones();
 
-	const SALARIO_BRUTO = SALARIO_4_HORAS + SALARIO_5_HORAS + SALARIO_6_5_HORAS + SALARIO_7_HORAS + SALARIO_8_HORAS + SALARIO_COACHING + SALARIO_FESTIVO_ESPECIAL + SALARIO_EVENTO + VACACIONES;
+	const SALARIO_BRUTO = SALARIO_4_HORAS + SALARIO_5_HORAS + SALARIO_6_5_HORAS + SALARIO_7_HORAS + SALARIO_8_HORAS + SALARIO_8_25_HORAS + SALARIO_COACHING + SALARIO_FESTIVO + SALARIO_FESTIVO_ESPECIAL + SALARIO_EVENTO + VACACIONES;
 
 	return {
 		SALARIO_4_HORAS,
@@ -47,7 +53,9 @@ const calcularSalarioBruto = () => {
 		SALARIO_6_5_HORAS,
 		SALARIO_7_HORAS,
 		SALARIO_8_HORAS,
+		SALARIO_8_25_HORAS,
 		SALARIO_COACHING,
+		SALARIO_FESTIVO,
 		SALARIO_FESTIVO_ESPECIAL,
 		SALARIO_EVENTO,
 		VACACIONES,
@@ -61,26 +69,26 @@ const calcularCotizaciones = (salarioBruto) => {
 
 	// Tasas de cotización
 
-	const CONTINGENCIAS_COMUNES = 0.047;
-	const DESEMPLEO = 0.0155;
-	const FORMACION_PROFESIONAL = 0.001;
-	const MECANISMO_EQUIDAD_INTERGENERACIONAL = 0.0013;
+	const PORCENTAJE_CONTINGENCIAS_COMUNES = 0.047;
+	const PORCENTAJE_DESEMPLEO = 0.0155;
+	const PORCENTAJE_FORMACION_PROFESIONAL = 0.001;
+	const PORCENTAJE_MECANISMO_EQUIDAD_INTERGENERACIONAL = 0.0013;
 
 	// Cálculo de cotizaciones
 
-	const COTIZACION_CONTINGENCIAS_COMUNES = salarioBruto * CONTINGENCIAS_COMUNES;
-	const COTIZACION_DESEMPLEO = salarioBruto * DESEMPLEO;
-	const COTIZACION_FORMACION_PROFESIONAL = salarioBruto * FORMACION_PROFESIONAL;
-	const COTIZACION_MECANISMO_EQUIDAD_INTERGENERACIONAL = salarioBruto * MECANISMO_EQUIDAD_INTERGENERACIONAL;
+	const CONTINGENCIAS_COMUNES = salarioBruto * PORCENTAJE_CONTINGENCIAS_COMUNES;
+	const DESEMPLEO = salarioBruto * PORCENTAJE_DESEMPLEO;
+	const FORMACION_PROFESIONAL = salarioBruto * PORCENTAJE_FORMACION_PROFESIONAL;
+	const MECANISMO_EQUIDAD_INTERGENERACIONAL = salarioBruto * PORCENTAJE_MECANISMO_EQUIDAD_INTERGENERACIONAL;
 
-	const TOTAL_COTIZACIONES = COTIZACION_CONTINGENCIAS_COMUNES + COTIZACION_DESEMPLEO + COTIZACION_FORMACION_PROFESIONAL + COTIZACION_MECANISMO_EQUIDAD_INTERGENERACIONAL;
+	const TOTAL = CONTINGENCIAS_COMUNES + DESEMPLEO + FORMACION_PROFESIONAL + MECANISMO_EQUIDAD_INTERGENERACIONAL;
 
 	return {
-		COTIZACION_CONTINGENCIAS_COMUNES,
-		COTIZACION_DESEMPLEO,
-		COTIZACION_FORMACION_PROFESIONAL,
-		COTIZACION_MECANISMO_EQUIDAD_INTERGENERACIONAL,
-		TOTAL_COTIZACIONES
+		CONTINGENCIAS_COMUNES,
+		DESEMPLEO,
+		FORMACION_PROFESIONAL,
+		MECANISMO_EQUIDAD_INTERGENERACIONAL,
+		TOTAL
 	};
 }
 
@@ -90,16 +98,13 @@ const calcularVacaciones = () => {
 	const DIAS_SEMANA = 7;
 	const DIAS_VACACIONES = document.getElementById('vacaciones');
 	const HORAS_SEMANA_CONTRATO = document.getElementById('horas-semana');
+	const HORAS_VACACIONES = (HORAS_SEMANA_CONTRATO.value / DIAS_SEMANA) * DIAS_VACACIONES.value;
+
 	let remuneracionVacaciones;
 
-	if (DIAS_VACACIONES.value === 0) {
-		remuneracionVacaciones = 0;
-	}
-
-	else {
-		const HORAS_VACACIONES = (HORAS_SEMANA_CONTRATO.value / DIAS_SEMANA) * DIAS_VACACIONES.value;
-		remuneracionVacaciones = HORAS_VACACIONES * PRECIO_HORA;
-	}
+	DIAS_VACACIONES.value === 0
+		? remuneracionVacaciones = 0
+		: remuneracionVacaciones = HORAS_VACACIONES * PRECIO_HORA;
 
 	return remuneracionVacaciones;
 }
@@ -112,18 +117,21 @@ const calcularNomina = () => {
 
 	const COTIZACION = calcularCotizaciones(SALARIO_BRUTO.SALARIO_BRUTO);
 
-	const SALARIO_NETO = SALARIO_BRUTO.SALARIO_BRUTO - COTIZACION.TOTAL_COTIZACIONES;
+	const SALARIO_NETO = SALARIO_BRUTO.SALARIO_BRUTO - COTIZACION.TOTAL;
 
 	OUTPUT.innerHTML =
-		`<p>Salario desglosado:</p>
+		`
+			<p>Salario desglosado:</p>
 
 			<p>-4 horas: ${SALARIO_BRUTO.SALARIO_4_HORAS} €.</p>
 			<p>-5 horas: ${SALARIO_BRUTO.SALARIO_5_HORAS} €.</p>
 			<p>-6.5 horas: ${SALARIO_BRUTO.SALARIO_6_5_HORAS} €.</p>
 			<p>-7 horas: ${SALARIO_BRUTO.SALARIO_7_HORAS} €.</p>
 			<p>-8 horas: ${SALARIO_BRUTO.SALARIO_8_HORAS} €.</p>
+			<p>-8.25 horas: ${SALARIO_BRUTO.SALARIO_8_25_HORAS} €.</p>
 			<p>-Coaching: ${SALARIO_BRUTO.SALARIO_COACHING} €.</p>
-			<p>-Festivo especial: ${SALARIO_BRUTO.SALARIO_FESTIVO_ESPECIAL} €.</p>
+			<p>-Festivos: ${SALARIO_BRUTO.SALARIO_FESTIVO} €.</p>
+			<p>-Festivos especiales: ${SALARIO_BRUTO.SALARIO_FESTIVO_ESPECIAL} €.</p>
 			<p>-Eventos: ${SALARIO_BRUTO.SALARIO_EVENTO} €.</p>
 			<p>-Vacaciones: ${SALARIO_BRUTO.VACACIONES} €.</p>
 
@@ -131,11 +139,11 @@ const calcularNomina = () => {
 
 			<p>--Cotizaciones--</p>
 
-			<p>-Contingencias comunes: -${COTIZACION.COTIZACION_CONTINGENCIAS_COMUNES} €.</p>
-			<p>-Desempleo: -${COTIZACION.COTIZACION_DESEMPLEO} €.</p>
-			<p>-Formación profesional: -${COTIZACION.COTIZACION_FORMACION_PROFESIONAL} €.</p>
-			<p>-Mecanismo de equidad intergeneracional: -${COTIZACION.COTIZACION_MECANISMO_EQUIDAD_INTERGENERACIONAL} €.</p>
-			<p>Total cotizaciones: -${COTIZACION.TOTAL_COTIZACIONES} €.</p>
+			<p>-Contingencias comunes: -${COTIZACION.CONTINGENCIAS_COMUNES} €.</p>
+			<p>-Desempleo: -${COTIZACION.DESEMPLEO} €.</p>
+			<p>-Formación profesional: -${COTIZACION.FORMACION_PROFESIONAL} €.</p>
+			<p>-Mecanismo de equidad intergeneracional: -${COTIZACION.MECANISMO_EQUIDAD_INTERGENERACIONAL} €.</p>
+			<p>Total cotizaciones: -${COTIZACION.TOTAL} €.</p>
 
 			<p>Salario neto: ${SALARIO_NETO} €.</p>
 		`;
